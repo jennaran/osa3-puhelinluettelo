fc485c6d19962ec9c5112aa5ec0b1ac129e1ec73
Jenna <jenna.rantanen@helsinki.fi>
2020-06-26 18:48:27 +0300

    build

---
+++ build/precache-manifest.08875e3a334514337d75d509e9ee3d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a8b2fbf0fd3d67612bedeed509eaf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "562bb629bff380284038",
+    "url": "/static/css/main.4b6e3f73.chunk.css"
+  },
+  {
+    "revision": "3cecff66644f73fc80c9",
+    "url": "/static/js/2.357f7ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.357f7ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562bb629bff380284038",
+    "url": "/static/js/main.07c7d445.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
